9a7510d5e081fe62b108b001908f97f518dd5c9d
Deenie Wallace <deenie.wallace@u.group>
2020-09-30 17:19:06 -0700

    added api key

--- index.vr.js
+++ index.vr.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { View, Text, Pano, AppRegistry, asset } from 'react-vr';
 
+const api_key = 'cf97c089528d6fe1a93adb4316bf49bb';
+
 class WeatherSimulator extends Component {
   render() {
     return (
@@ -13,3 +15,5 @@ class WeatherSimulator extends Component {
 };
 
 AppRegistry.registerComponent('WeatherSimulator', () => WeatherSimulator);
+
+// Amsterdam photo credit: ruei_ke <a href="http://www.flickr.com/photos/7380590@N03/8517676763">Amsterdam</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">(license)</a>
